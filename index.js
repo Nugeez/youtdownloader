5663b96134f85b98c0b9f263c7d579443c7f2950
Oluwasegun Ariyo <oluwasegun.ariyo@verraki.com>
2019-12-17 10:57:53 +0100

    all seems done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const dotenv = require('dotenv').config();
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +12,8 @@ const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/downloadmp3', (req,res) => {
 	var url = req.query.url;
